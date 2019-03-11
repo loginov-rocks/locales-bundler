36e818d1723429a6f7a3cda5f28d0efa01b260d3
Danila Loginov <danila@loginov.rocks>
2019-03-11 04:45:09 +0300

    implement locales bundler

---
+++ src/cli.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const localesBundler = require('./localesBundler');
+
+const getFlagValue = (argument) => {
+  const i = process.argv.indexOf(`-${argument}`);
+
+  if (i === -1) {
+    return null;
+  }
+
+  return process.argv[i + 1];
+};
+
+const omit = getFlagValue('o') || '';
+const pattern = getFlagValue('p') || '*.json';
+const destination = getFlagValue('d') || 'dist';
+
+let source = getFlagValue('s');
+
+if (!source) {
+  source = process.argv[2];
+
+  if (!source) {
+    throw new Error('Source path is not specified');
+  }
+}
+
+const contentsByFileNames = localesBundler(path.resolve(source), pattern, omit);
+
+Object.keys(contentsByFileNames).forEach((fileName) => {
+  fs.writeFileSync(path.resolve(destination, fileName),
+      JSON.stringify(contentsByFileNames[fileName]));
+});
---
+++ src/localesBundler.js
@@ -0,0 +1,41 @@
+const deepAssign = require('deep-assign');
+const fs = require('fs');
+const glob = require('glob');
+const objectPath = require('object-path');
+const path = require('path');
+
+/**
+ * Locales bundler.
+ * @param {string} source
+ * @param {string} pattern
+ * @param {string} [omit='']
+ * @return {Object} Contents bundled by file names.
+ */
+module.exports = (source, pattern, omit = '') => {
+  const files = glob.sync(path.join(source, pattern));
+
+  const contentsByFileNames = {};
+
+  files.forEach((filePath) => {
+    let relativePath = path.relative(source, path.dirname(filePath)).
+        replace(new RegExp(omit, 'g'), '');
+
+    // Replace backslashes for Windows paths.
+    relativePath = relativePath.replace(/\\/g, '/');
+    // Trim slashes.
+    relativePath = relativePath.replace(/^\/|\/$/g, '');
+
+    const fileName = path.basename(filePath);
+
+    const content = {};
+    const json = JSON.parse(fs.readFileSync(filePath));
+
+    objectPath.set(content, relativePath.replace(/\//g, '.'), json);
+
+    contentsByFileNames[fileName] = contentsByFileNames[fileName] || {};
+
+    deepAssign(contentsByFileNames[fileName], content);
+  });
+
+  return contentsByFileNames;
+};
